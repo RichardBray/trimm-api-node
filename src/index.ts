fb312bbcfb0b970b94bcaee9274de5598f60a042
Richard Bray <richardbray89@gmail.com>
2021-10-13 16:08:36 +0100

    build: add vite

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-function test(): string {
-    return 'test';
+import express from "express";
+import { PORT } from "./config";
+import pino from "pino";
+
+const app = express();
+const logger = pino();
+app.use(express.json()); // Parse response payload to json
+
+app.get("/test", (req, res) => {
+  res.status(200).send("Hello 42211");
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`);
+  });
 }
+
+export const viteNodeApp = app;
