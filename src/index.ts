50d6ba1c5167fc2d7e12222b82f24df9b254d13e
Richard Bray <richardbray89@gmail.com>
2021-10-18 14:24:43 +0100

    build: replace vite with nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
 import express from "express";
-import logger from "./utils/logger";
+import logger from "./utils/logger.js";
 
-import { PORT } from "./config";
+import { PORT } from "./config/index.js";
 
 const app = express();
 
 app.use(express.json()); // Parse response payload to json
 
 app.get("/test", (req, res) => {
-  logger.info("testing");
-  res.status(200).send("Hello 41122");
+  logger.warn("testing");
+  res.status(200).send("Hello 42211");
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`);
-  });
-}
-
-export const viteNodeApp = app;
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+});
