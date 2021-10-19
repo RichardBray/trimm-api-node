608bf98c43c90dc99a370faf5c7281cf56f61f13
Richard Bray <richardbray89@gmail.com>
2021-10-19 21:20:34 +0100

    feat: add registration, logout & verify auth endpoints

--- src/components/Auth/AuthController.ts
+++ src/components/Auth/AuthController.ts
@@ -1,49 +1,73 @@
 import { Request, Response } from "express";
-import "cross-fetch/polyfill";
-import * as AWS from "aws-sdk/global";
-import { CognitoUserPool, CognitoUser, AuthenticationDetails, CognitoUserSession } from "amazon-cognito-identity-js";
+import "cross-fetch/dist/node-polyfill.js";
+import * as AWS from "aws-sdk/global.js";
+import {
+  CognitoUserPool,
+  CognitoUser,
+  AuthenticationDetails,
+  CognitoUserSession,
+  CognitoUserAttribute,
+} from "amazon-cognito-identity-js";
 
 import * as config from "../../config.js";
 import logger from "../../utils/logger.js";
 
 class AuthController {
-  static #poolData = {
-    UserPoolId: config.cognito.userPoolId,
-    ClientId: config.cognito.clientId,
-  };
+  static login(req: Request, res: Response) {
+    try {
+      logger.info("Attempting user login");
 
-  static #createUserPool(): CognitoUserPool {
-    return new CognitoUserPool(AuthController.#poolData);
+      if (!req.body.password) {
+        throw new Error("Password is required");
+      }
+
+      const authData = {
+        Username: req.body.username,
+        Password: req.body.password,
+      };
+      const authDetails = new AuthenticationDetails(authData);
+      const cognitoUser = AuthController.#createCognitoUser(req);
+
+      cognitoUser.authenticateUser(authDetails, {
+        onSuccess: (result) => {
+          const accessToken = result.getAccessToken().getJwtToken();
+          // AuthController.#configureRefreshCredentials(result);
+          res.status(200).send({
+            accessToken,
+          });
+        },
+        onFailure: (err) => {
+          logger.error(err);
+          res.status(401).send(err);
+        },
+      });
+    } catch (err) {
+      logger.error(err);
+      res.status(400).send(err);
+    }
   }
 
-  static login(req: Request, res: Response) {
+  static #createUserPool(): CognitoUserPool {
+    const poolData = {
+      UserPoolId: config.cognito.userPoolId as string,
+      ClientId: config.cognito.clientId as string,
+    } as const;
+
+    return new CognitoUserPool(poolData);
+  }
+
+  static #createCognitoUser(req: Request): CognitoUser {
+    if (!req.body.username) {
+      throw new Error("Username is required");
+    }
+
     const userPool = AuthController.#createUserPool();
     const userData = {
       Username: req.body.username,
       Pool: userPool,
     };
-    const cognitoUser = new CognitoUser(userData);
-    const authData = {
-      Username: req.body.username,
-      Password: req.body.password,
-    };
-    const authDetails = new AuthenticationDetails(authData);
 
-    logger.info("Attempting user login");
-
-    cognitoUser.authenticateUser(authDetails, {
-      onSuccess: (result) => {
-        const accessToken = result.getAccessToken().getJwtToken();
-        AuthController.#configureRefreshCredentials(result);
-        res.status(200).send({
-          accessToken,
-        });
-      },
-      onFailure: (err) => {
-        logger.error(err);
-        res.status(401).send(err);
-      },
-    });
+    return new CognitoUser(userData);
   }
 
   static #configureRefreshCredentials(result: CognitoUserSession) {
@@ -62,18 +86,71 @@ class AuthController {
       if (error) {
         logger.error(error);
       } else {
-        logger.info("Successfully logged in!");
+        logger.info("User login successful");
       }
     });
   }
 
-  static register() {
-    AuthController.#createUserPool();
+  static register(req: Request, res: Response) {
+    logger.info("Attempting user registration");
+
+    const userPool = AuthController.#createUserPool();
+    const attributeList = [];
+    const dataEmail = {
+      Name: "email",
+      Value: req.body.username,
+    };
+    const attributeEmail = new CognitoUserAttribute(dataEmail);
+    attributeList.push(attributeEmail);
+
+    userPool.signUp(
+      req.body.username,
+      req.body.password,
+      attributeList,
+      [],
+      (err, result) => {
+        if (err) {
+          logger.error(err);
+          res.status(400).send(err);
+        } else {
+          logger.info("User registration successful");
+          res.status(200).send(result);
+        }
+      }
+    );
   }
 
-  // TODO
-  static logout() {
-    return false;
+  static verifyRegistration(req: Request, res: Response) {
+    logger.info("Attempting user verification");
+
+    const cognitoUser = AuthController.#createCognitoUser(req);
+
+    cognitoUser.confirmRegistration(
+      req.body.verificationCode,
+      true,
+      (err: Error, result: string) => {
+        if (err) {
+          logger.error(err);
+          res.status(400).send(err);
+        } else {
+          logger.info("User verification successful");
+          res.status(200).send(result);
+        }
+      }
+    );
+  }
+
+  static logout(req: Request, res: Response) {
+    logger.info("Attempting logout");
+    try {
+      const cognitoUser = AuthController.#createCognitoUser(req);
+
+      cognitoUser.signOut();
+      logger.info("Logout successful");
+    } catch (err) {
+      logger.error(err);
+      res.status(400).send(err);
+    }
   }
 
   // TODO
--- src/components/Auth/AuthRouter.ts
+++ src/components/Auth/AuthRouter.ts
@@ -3,7 +3,11 @@ import AuthController from "./AuthController.js";
 
 const authRouter = express.Router();
 
+authRouter.use(express.json());
+
 authRouter.post("/login", AuthController.login);
 authRouter.post("/register", AuthController.register);
+authRouter.post("/verify", AuthController.verifyRegistration);
+authRouter.post("/logout", AuthController.logout);
 
 export default authRouter;
